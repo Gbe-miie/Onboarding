867beb9ea94abd23c2c075862062fdf93b244af2
Gbe-miie <143811525+Gbe-miie@users.noreply.github.com>
2023-09-25 20:29:17 +0000

    folio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -42,16 +42,6 @@ const Portfolio = () => {
             alt={imageAltText}
           />
         </div>
-        <div className="container">
-          {projectList.map((project) => (
-            <div className="box" key={project.title}>
-              <a href={project.url} target="_blank" rel="noopener noreferrer">
-                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
-              </a>
-              <p className="small">{project.description}</p>
-            </div>
-          ))}
-        </div>
       </div>
     </section>
   );
