e4d147c6cc054cb2536261ed74a95cc557d00cbe
Gbe-miie <143811525+Gbe-miie@users.noreply.github.com>
2023-09-25 19:15:49 +0000

    Enter

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,20 +24,12 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
-
+  "I am Adewunmi, a student of Yaba College of Technology, studying Computer Science and also a Data Analyst";
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+const skillsList = ["Data Analysis", "Business Analysis", "Student", "Community Development"];
 
 /**
  * Use this to give more information about what you are passionate about,
